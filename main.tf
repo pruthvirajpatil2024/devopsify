74a64649b65cd52286a093ee612ce6db7edae234
Pruthviraj Patil <137247110+pruthvirajpatil2024@users.noreply.github.com>
2025-06-07 07:39:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "google" {
   credentials = file(var.gcp_credentials)
-  project     = "tt-dev-001"
+  project     = "stalwart-fx-456910-c0"
   region      = "us-central1"
 }
 
 resource "google_storage_bucket" "my-bucket" {
   name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
+  project                  = "stalwart-fx-456910-c0"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
@@ -14,7 +14,7 @@ resource "google_storage_bucket" "my-bucket" {
 #
 resource "google_storage_bucket" "my-bucket2" {
   name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
+  project                  = "stalwart-fx-456910-c0"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
